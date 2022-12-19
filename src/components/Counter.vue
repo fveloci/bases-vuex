6af28246451bc4514996a7648bd19eb7edc9f5a3
Franco Veloci <fveloci@mobeats.io>
2022-12-19 15:56:22 -0300

    First commit

---
+++ src/components/Counter.vue
@@ -0,0 +1,42 @@
+<!-- eslint-disable vue/multi-word-component-names -->
+<template>
+  <h1>Counter - Vuex</h1>
+  <h2>Direct access {{$store.state.counter.count}}</h2>
+  <h2>Computed {{ countComputed }}</h2>
+
+  <button @click="increment">+1</button>
+  <button @click="incrementBy(5)">+5</button>
+  <button @click="incrementRandomInt" :disabled="isLoading">Random</button>
+
+  <h1>mapState</h1>
+  <h2>mapState: {{count}}</h2>
+  <h2>lastMutation: {{ lastMutation }}</h2>
+
+  <h2>Direct getter: {{ $store.getters['counter/squareCount'] }}</h2>
+</template>
+
+<script>
+import { mapActions, mapState } from 'vuex'
+
+export default {   
+    computed: {
+        countComputed() {
+            return this.$store.state.counter.count
+        },
+        ...mapState( 'counter', ['count', 'lastMutation', 'isLoading'])
+        // ...mapState({
+        //     count: state => state.count,
+        //     lastMutation: state => state.lastMutation
+        // })
+    },
+    methods: {
+        increment() {
+            this.$store.commit('counter/increment')
+        },
+        incrementBy(val) {
+            this.$store.commit('counter/incrementBy', val)
+        },
+        ...mapActions( 'counter' , ['incrementRandomInt'])
+    }
+}
+</script>
